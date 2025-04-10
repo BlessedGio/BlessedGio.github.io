1ff70e88e89fdf9790812e9068602b6babea4612
BB-BusyBox <83779967+BB-BusyBox@users.noreply.github.com>
2025-04-10 23:55:47 +0200

    tt

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3300192104"
+    serviceWorkerVersion: "2228827902"
   }
 });
